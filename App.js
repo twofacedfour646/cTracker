d1b6a83d82e147329b0705b503ff6ebe9fc83f55
twofacedfour646 <mdgubler8@gmail.com>
2022-11-06 09:27:13 -0500

    Navigation

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return <SafeAreaView style={styles.container}>
+          <View style={styles.divider}>
+            <Text style={{ fontSize: "35pt", fontWeight: "bold",}}>Flare Up Tracker</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Search"
+            />
+          </View>
+
+          <View style={styles.bottom}>
+            <TouchableOpacity style={styles.addBtn} onPress={() => navigation.navigate('Flare Up Form')}>
+              <Text style={{ fontSize: "40pt", fontWeight: "bold", color: "#2196f3" }}>+</Text>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+}
+
+function FlareUpForm() {
+  return <SafeAreaView style={styles.container}>
+          <View style={styles.divider}>
+            <Text style={{ fontSize: "35pt", fontWeight: "bold",}}>Form</Text>
+          </View>
+        </SafeAreaView>
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Flare Up Tracker" component={HomeScreen}/>
+        <Stack.Screen name="Flare Up Form" component={FlareUpForm}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: "5pt",
+    borderColor: "#aaa",
+    marginTop: 10
+  },
+
+  divider: {
+    // flex: 0.5
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "space-between"
   },
+
+  addBtn: {
+    borderWidth: 4,
+    alignItems: "center",
+    justifyContent: "center",
+    width: 75,
+    height: 75,
+    borderColor: "#2196f3",
+    borderRadius: "20pt",
+    boxShadow: "10pt 10pt 5pt 12pt black"
+  },
+
+  bottom: {
+    // flex: 0.5
+  }
+
 });
